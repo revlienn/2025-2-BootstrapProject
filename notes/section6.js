416f57f815910f9823866541937f19abf05ec095
revlienn <revnathalia@gmail.com>
2025-06-26 11:24:58 +0100

    C40

---
+++ notes/section6.js
@@ -0,0 +1,19 @@
+// COURSE 40
+/*
+npm init -y
+npm i sass
+
+package.json
+    "sass:build": "sass --no-source-map scss:css",
+        source map is for debugging, rarely used
+        this compile scss to css
+    "sass:watch": "sass --no-source-map --watch scss:css"
+        recompile everytime you modify the sass file
+    npm run sass:build
+        now you get the css file
+    npm run sass:watch
+        so it recompile automatically
+
+index.html
+    add stylesheet, styles.css
+*/
